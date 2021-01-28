669d750d5bed1cccebaaa2d472b1c563e2aaa86f
doniev <doniev@localhost.localdomain>
2021-01-28 11:09:58 +0500

    Main Menu Bug Fix

--- spectreuz/movies/models.py
+++ spectreuz/movies/models.py
@@ -2,15 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Slider(models.Model):
-
-    slider_title = models.CharField(max_length=100)
-    caption = models.TextField()
-    slider_image = models.ImageField(upload_to="images/", blank=True)
-
-    def __str__(self):
-        
-        return str(self.id) + self.slider_title
 
 
 
@@ -41,28 +32,7 @@ class Movies(models.Model):
         return self.caption     
 
 
-class Premyera(models.Model):
 
-    premyera_caption = models.CharField(max_length=100)
-    premyera_image = models.ImageField(upload_to ='images/premyera/')  
-
-    def __str__(self):
-        
-        return str(self.id) + self.premyera_caption
-
-
-class PostsForMainMenu(models.Model):
-    
-    post_title = models.CharField(max_length=40)
-    post_author = models.CharField(max_length=50)
-    post_date = models.DateTimeField(auto_now_add=True, blank=True)
-    post_description = models.TextField()
-    post_full_description = models.TextField()
-    post_image = models.ImageField(upload_to ='images/postsformainmenu/')
-
-    def __str__(self):
-
-        return self.post_title + " | " + self.post_author
 
 class CardsForTrailer(models.Model):        
     card_caption = models.CharField(max_length=200)
